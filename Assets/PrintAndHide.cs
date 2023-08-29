544539e72885ed5f830456cdc4ab31432222802a
2695216399 <yuanqing.wang-1@student.uts.edu.au>
2023-08-29 18:00:58 +1000

    Week4

--- Assets/PrintAndHide.cs
+++ Assets/PrintAndHide.cs
@@ -6,16 +6,25 @@ public class PrintAndHide : MonoBehaviour
 {
     int i = 3;
     public Renderer rend;
+    int Rend;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(this.gameObject.name);
-        Debug.Log(i++);
+       Rend = Random.Range(150, 251);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(this.gameObject.name+":" + ++i);
+
+        if (gameObject.tag == "Red" && i==100)
+        {
+            gameObject.SetActive(false);
+        }
+        if (gameObject.tag == "Blue" && i == Rend)
+        {
+            rend.enabled = false;
+        }
     }
 }
